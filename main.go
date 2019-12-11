24b18eb25a817a729dcb105907638d9ec872988d
phillmac <phillmac@users.noreply.github.com>
2019-12-12 09:05:17 +1100

    Use torrents := map[string]*Torrent{}

--- main.go
+++ main.go
@@ -75,7 +75,7 @@ func setInterval(someFunc func(), milliseconds int, async bool) chan bool {
 
 }
 
-var torrents []Torrent
+torrents := map[string]*Torrent{}
 
 func updateStats() {
 	util.Log("Updating stats")
