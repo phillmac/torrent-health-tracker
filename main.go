0aeff6576e7dd3f87a52f075deeec90bddf13a7d
phillmac <phillmac@users.noreply.github.com>
2019-12-11 21:37:02 +1100

    Move updateStats

--- main.go
+++ main.go
@@ -4,123 +4,116 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
-	"time"
 	"os"
-	
+	"time"
+
 	"github.com/nektro/go-util/util"
 	etc "github.com/nektro/go.etc"
 	"github.com/rakyll/statik/fs"
-
-
-
-
 )
 
-
 type DHTData struct {
-	Hash     	string 	`json:"infoHash"`
-	Name     	string
-	Peers    	int
-	ScrapedDate 	int 	`json:"scraped_date"`
+	Hash        string `json:"infoHash"`
+	Name        string
+	Peers       int
+	ScrapedDate int `json:"scraped_date"`
 }
 
 type TrackerData struct {
-	Hash     	string 	`json:"infoHash"`
-	Seeders  	int 	`json:"complete"`
-	Leechers 	int 	`json:"incomplete"`
-	ScrapedDate 	int 	`json:"scraped_date"`
+	Hash        string `json:"infoHash"`
+	Seeders     int    `json:"complete"`
+	Leechers    int    `json:"incomplete"`
+	ScrapedDate int    `json:"scraped_date"`
 }
 
 type Torrent struct {
-	Hash     	string	`json:"_id"`
-	Name     	string
-	Size     	int	`json: "size_bytes"`
-	Link		string
-	Seeders  	int
-	Leechers 	int
-	DHTData 	DHTData
-	TrackerData 	map[string]*TrackerData
+	Hash        string `json:"_id"`
+	Name        string
+	Size        int `json: "size_bytes"`
+	Link        string
+	Seeders     int
+	Leechers    int
+	DHTData     DHTData
+	TrackerData map[string]*TrackerData
 }
 
 func setInterval(someFunc func(), milliseconds int, async bool) chan bool {
 
-        // How often to fire the passed in function 
-        // in milliseconds
-        interval := time.Duration(milliseconds) * time.Millisecond
+	// How often to fire the passed in function
+	// in milliseconds
+	interval := time.Duration(milliseconds) * time.Millisecond
 
-        // Setup the ticker and the channel to signal
-        // the ending of the interval
-        ticker := time.NewTicker(interval)
-        clear := make(chan bool)
+	// Setup the ticker and the channel to signal
+	// the ending of the interval
+	ticker := time.NewTicker(interval)
+	clear := make(chan bool)
 
-        // Put the selection in a go routine
-        // so that the for loop is non-blocking
-        go func() {
-            for {
+	// Put the selection in a go routine
+	// so that the for loop is non-blocking
+	go func() {
+		for {
 
-                select {
-                case <-ticker.C:
-                    if async {
-                        // This won't block
-                        go someFunc()
-                    } else {
-                        // This will block
-                        someFunc()
-                    }
-                case <-clear:
-                    ticker.Stop()
-                    return
-                }
+			select {
+			case <-ticker.C:
+				if async {
+					// This won't block
+					go someFunc()
+				} else {
+					// This will block
+					someFunc()
+				}
+			case <-clear:
+				ticker.Stop()
+				return
+			}
 
-            }
-        }()
+		}
+	}()
 
-        // We return the channel so we can pass in 
-        // a value to it to clear the interval
-        return clear
+	// We return the channel so we can pass in
+	// a value to it to clear the interval
+	return clear
 
 }
 
+var torrents []Torrent
+
+func updateStats() {
+	util.Log("Updating stats")
+	resp, err := http.Get("https://phillm.net/libgen-stats.php")
+	if err != nil {
+		util.LogError("Failed to fetch stats", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		util.LogError("Failed to read body", err.Error())
+		return
+	}
+	json.Unmarshal([]byte(body), &torrents)
+
+	for _, torrent := range torrents {
+		seeders := 0
+		leechers := 0
+		for k, v := range torrent.TrackerData {
+			if v.Seeders > seeders {
+				seeders = v.Seeders
+				leechers = v.leechers
+			}
+		}
+		torrent.Seeders = seeders
+		torrent.Leechers = leechers
+	}
+
+	util.Log("Torrent count:", len(torrents))
+}
 
 func main() {
 
-	var torrents []Torrent
-	
-	func updateStats () {
-		util.Log("Updating stats")
-		resp, err := http.Get("https://phillm.net/libgen-stats.php")
-		if err != nil {
-			util.LogError("Failed to fetch stats", err.Error())
-			return
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			util.LogError("Failed to read body", err.Error())
-			return
-		}
-		json.Unmarshal([]byte(body), &torrents)
-		
-		for _,torrent  := range torrents {
-			seeders := 0
-			leechers := 0
-			for k, v := range torrent.TrackerData {
-				if v.Seeders > seeders {
-					seeders = v.Seeders
-					leechers = v.leechers
-				}
-			}
-			torrent.Seeders = seeders
-			torrent.Leechers = leechers
-		}
-		
-		util.Log("Torrent count:", len(torrents))
-	}
-	
 	setInterval(func() {
 		updateStats()
-	}, 1800 * 1000, true)
-	
+	}, 1800*1000, true)
 
 	etc.MFS.Add(http.Dir("www"))
 
@@ -137,5 +130,3 @@ func main() {
 
 	http.ListenAndServe(":80", nil)
 }
-
-
