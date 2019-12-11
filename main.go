7d1dde854024c7a07a249357731efbd20ea6a3db
phillmac <phillmac@users.noreply.github.com>
2019-12-12 10:36:57 +1100

    Debug

--- main.go
+++ main.go
@@ -75,7 +75,7 @@ func setInterval(someFunc func(), milliseconds int, async bool) chan bool {
 
 }
 
-func updateStats(map[string]*Torrent torrents) {
+func updateStats(torrents map[string]*Torrent{}) {
 	util.Log("Updating stats")
 	resp, err := http.Get("https://phillm.net/libgen-stats.php")
 	if err != nil {
