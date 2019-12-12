7c0505ef0b17e805a585b3adc14861e9aa4cea66
phillmac <phillmac@users.noreply.github.com>
2019-12-12 12:46:06 +1100

    Debug

--- main.go
+++ main.go
@@ -123,9 +123,9 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		results := map[string]*Torrent{}
+		results := map[string]Torrent
 		for _, t := range torrents {
-			results[t.Hash] = &t
+			results[t.Hash] = t
 		}
 		etc.WriteHandlebarsFile(r, w, "/index.hbs", map[string]interface{}{
 			"torrents": results,
